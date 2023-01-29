7462647587c7e43e815e69c4a373b723808f9f46
Jorge <martinezca.jorge@gmail.com>
2023-01-29 18:41:19 +0100

    save

---
+++ main.py
@@ -0,0 +1,30 @@
+project_id = serverless-376216
+bucket-name = edem-serverless-bucket1
+
+
+gcloud builds submit --tag 'gcr.io/serverless-376216/dataflow/edem:latest' .
+
+
+gcloud dataflow flex-template build "gs://edem-serverless-bucket/dataflowtemplate.json" \
+  --image 'gcr.io/deft-epigram-375817/dataflow/edem:latest' \
+  --sdk-language "PYTHON" 
+
+
+
+gcloud dataflow flex-template run "edem-dataflow-job2" \
+    --template-file-gcs-location "gs://edem-serverless-bucket/dataflowtemplate.json" \
+    --region "europe-west4"
+
+
+python edemDeviceData.py \
+    --algorithm RS256 \
+    --cloud_region europe-west1 \
+    --device_id edemDevice \
+    --private_key_file rsa_private.pem \
+    --project_id deft-epigram-375817 \
+    --registry_id edemRegistry
+
+
+gcloud dataflow flex-template build "gs://edem-serverless-bucket1/dataflowtemplate.json" \
+  --image "gcr.io/serverless-376216/dataflow/edem:latest" \
+  --sdk-language "PYTHON" 
